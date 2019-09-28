047be4819c1b77cb9ae5c0f36b293d36147fd139
yunggenius <yunggenius4@gmail.com>
2019-09-28 19:56:32 +0100

    index header

--- Index.php
+++ Index.php
@@ -11,22 +11,8 @@
 
 <body>
   <div class="header">
-    <div class="logo">
-    <a href="index.html"><img
-        src="https://res.cloudinary.com/enema/image/upload/v1569433441/Ariadne_Class_pnlixb.png"
-        style="width: 110px;" alt="logo">
-    </a>
-    </div>
-    <div class="topnav" id="myTopnav">
-      <a href="javascript:void(0);" class="icon" onclick="myFunction()"><img src="https://res.cloudinary.com/siyfa/image/upload/v1568922461/ovqrbsa6t7nhghflejve.png" style="width: 30px;">
-        </a>
-        <a href="login.html">Login</a>
-        <a href="#">Contact Us</a>
-        <a href="createclass.html">Create Class</a>
-        <a href="accessclass.html">Access Class</a>
-        <a href="sign-up.php">Sign Up</a>
-        <a href="index.html">Home</a>
-  </div>
+<?php 
+include "header.php" ;?>
   </div>  
       <script>
           function myFunction() {
