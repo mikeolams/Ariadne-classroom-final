ac03dfbabc583b626612e54f1ad26a33a3b4ba64
yunggenius <yunggenius4@gmail.com>
2019-09-28 17:26:31 +0100

    created header.php

---
+++ header.php
@@ -0,0 +1,31 @@
+<header>
+<div class="header">
+    <div class="logo">
+    <a href="index.html"><img
+        src="https://res.cloudinary.com/enema/image/upload/v1569433441/Ariadne_Class_pnlixb.png"
+        style="width: 110px;" alt="logo">
+    </a>
+    </div>
+    <div class="topnav" id="myTopnav">
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()"><img src="https://res.cloudinary.com/siyfa/image/upload/v1568922461/ovqrbsa6t7nhghflejve.png" style="width: 30px;">
+        </a>
+        <a href="sign up.html">Login</a>
+        <a href="#">Contact Us</a>
+        <a href="#">FAQ</a>
+        <a href="#">Courses</a>
+        <a href="about-us.html">How it works</a>
+        <a href="class.html">Create Class</a>
+        <a href="#">Home</a>
+  </div>
+  </div>  
+      <script>
+          function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+              x.className += " responsive";
+            } else {
+              x.className = "topnav";
+            }
+          }
+        </script>
+</header>
