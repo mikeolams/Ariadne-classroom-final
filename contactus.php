55694ec2ef691aabedc8408fc4acaacfda0f3646
yunggenius <yunggenius4@gmail.com>
2019-09-28 20:15:07 +0100

    contact us php

---
+++ contactus.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+	<title>Sign Up form</title>
+	<link rel="stylesheet" type="text/css" href="contactus.css">
+</head>
+<header>
+<div class="header">
+<?php 
+include "header.php" ;?>
+  </div>  
+      <script>
+          function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+              x.className += " responsive";
+            } else {
+              x.className = "topnav";
+            }
+          }
+        </script>
+</header>
+<body>
+  <div>
+    <h3>At Ariadne classroom, education is redefined with the world class accessories available for  your mentorship. We are located at  Start Innovation hub, Ibom e-libaray Uyo,  Akwa Ibom state or send us a mail today ... teamariadne80@gmail.com</h3>
+  </div>
+  
+</body>
+</html>
