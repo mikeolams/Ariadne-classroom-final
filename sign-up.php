704184e6c3b3b7a7b032af133f8ed5d8718efc0f
clearfountain <mjedet1001@gmail.com>
2019-10-04 09:46:28 +0100

    Updated signup back end validation

--- sign-up.php
+++ sign-up.php
@@ -4,8 +4,7 @@
     $con=mysqli_connect('localhost','root','1111111111') or die("Cannot connect to localhost");
     mysqli_select_db($con,'Ariadneclass') or die("Cannot Select Database");
 
-    //require 'includes/config.php';
-
+ // Function to sanitize user input
     function test_input($data){
         $data = trim($data);
         $data = stripslashes($data);
@@ -13,34 +12,36 @@
         return $data;
      }
 
-    $errors = 0;
+    //Function to set error message if user input is empty
+    function display_input_error(&$input,$session_name,$error_message) {
+        $input = trim($input);
+        if(  (isset($input) === true) && ($input === '') )  {
+            $_SESSION[$session_name] = $error_message;
+            return 1;
+        } else {
+            $input = test_input($input);
+            return 0;
+        }
+    }
+    
+    $fullname = $username = $email = $password = $cpassword = '';
+    $errors = $empty_email_flag = 0;
 
   if(isset($_POST['register'])) {
-    $errMsg = '';
 
-    // Get data from FROM
+    // Extract and store form data
     $_SESSION['fullname'] = $fullname = $_POST['fullname'];
     $_SESSION['username'] = $username = $_POST['username'];
     $_SESSION['email'] = $email = $_POST['email'];   
-    $password = $_POST['password'];
-    // $secretpin = $_POST['secretpin'];
+    $password = $_POST['password'];      
+    $cpassword = $_POST['password_confirm'];
       
-    if(empty($fullname)){
-        $_SESSION['empty_fullname'] = "Enter name";
-        $errors += 1;
-    } else {
-        $fullname = test_input($fullname);
-    }
+    $errors += display_input_error($fullname,'empty_fullname','-Enter name',$errors);
+    $errors += display_input_error($username,'empty_username','-Enter username',$errors);  
       
-    if(empty($username)){
-        $_SESSION['empty_username'] = "Enter username";
-        $errors += 1;
-    } else {
-        $username = test_input($username);
-    }
-      
+    //Set error message if email field is empty
     if(empty($email)){
-        $_SESSION['empty_email'] = "Enter your email";
+        $_SESSION['empty_email'] = "-Enter your email";
         $empty_email_flag = 1;
         $errors += 1;
     } else {
@@ -48,42 +49,67 @@
         $empty_email_flag = 0;
     }  
     
+    //Validate email format
     if((! filter_var($email,FILTER_VALIDATE_EMAIL)) && $empty_email_flag == 0){
-        $_SESSION['incorrect_email'] = "Invalid email format";
+        $_SESSION['incorrect_email'] = "-Invalid email format";
         $errors += 1;
     }
       
+    // Set error message if password field is empty
+    $password_length = strlen($password);
     if(empty($password)){
-        $_SESSION['blank_password'] = "Enter password";
+        $_SESSION['blank_password'] = "-Enter password";
+        $errors += 1;
+    } else if($password_length < 8){
+        $_SESSION['short_password'] = "-Password shouldn't be less than 8 characters";
+        $errors += 1;
+    } else if($password_length > 50){
+        $_SESSION['long_password'] = "-Password is too long";
+        $errors += 1;
+    } else if(!preg_match("#[0-9]+#",$password)){
+        $_SESSION['invalid_password'] = "-Password must contain at least one number,capital letter and lowercase letter";
+        $errors += 1;
+    } else if(!preg_match("#[A-Z]+#",$password)){
+        $_SESSION['invalid_password'] = "-Password must contain at least one number,capital letter and lowercase letter";
+        $errors += 1;
+    } else if(!preg_match("#[a-z]+#",$password)){
+        $_SESSION['invalid_password'] = "-Password must contain at least one number,capital letter and lowercase letter";
+        $errors += 1;
+    } else if($password !== $cpassword){
+        $_SESSION['password_mismatch'] = "-Passwords do not match";
         $errors += 1;
     }
       
+    // If there are no errors, check if email is already linked to an account, if there are errors redirect back to sign up page
     if($errors == 0){
         
-        $check_email = mysqli_prepare($con,"SELECT * from Ariadneclass WHERE email = ?");
+        $check_email = mysqli_prepare($con,"SELECT * from users WHERE email = ?");
         mysqli_stmt_bind_param($check_email,'s',$email);
         mysqli_stmt_execute($check_email);
         mysqli_stmt_store_result($check_email);
         $email_row = mysqli_stmt_num_rows($check_email);
         mysqli_stmt_close($check_email);
     
+        /* If email already exists, redirect back to sign up page else, hash password and store all user input */
         if($email_row > 0){
-                $_SESSION['emailerr'] = "A user with this email already exists";
+                $_SESSION['emailerr'] = "-A user with this email already exists";
                 if($con){mysqli_close($con);}
                 header('location: sign-up.php');
                 exit();
         }
       
         else {
-            $pword = password_hash("$pword",PASSWORD_DEFAULT);
-            $insert = mysqli_prepare($con,"INSERT INTO users (fullname, username, email, pword) VALUES (?,?,?,?)");
-            mysqli_stmt_bind_param($insert,'ssss',$fullname,$username,$email,$pword);
+            $password = password_hash("$password",PASSWORD_DEFAULT);
+            $insert = mysqli_prepare($con,"INSERT INTO users (fullname, username, email, password) VALUES (?,?,?,?)");
+            mysqli_stmt_bind_param($insert,'ssss',$fullname,$username,$email,$password);
             mysqli_stmt_execute($insert);
             mysqli_stmt_close($insert);
             
             $_SESSION['fullname'] = $fullname;
             $_SESSION['username'] = $username;
             $_SESSION['email'] = $email;
+            
+            $_SESSION['login_success'] = 'Signup successul, you may now login !';
             
             if($con){mysqli_close($con);}
             header('Location: login.php');
@@ -102,39 +128,6 @@ if($con){
 mysqli_close($con);
 }
 
-  /*  if($fullname == '')
-      $errMsg = 'Enter your fullname';
-    if($username == '')
-      $errMsg = 'Enter username';
-    if($password == '')
-      $errMsg = 'Enter password'; 
-    if($email == '')
-      $errMsg = 'Enter email';
-    // if($secretpin == '')
-    //   $errMsg = 'Enter a sercret pin number';
-
-    if($errMsg == ''){
-      try {
-        $stmt = $connect->prepare('INSERT INTO pdo (fullname, username, email, password) VALUES (:fullname, :username, :email, :password)');
-        $stmt->execute(array(
-          ':fullname' => $fullname,
-          ':username' => $username,
-          ':email' => $email,
-          ':password' => $password
-          // ':secretpin' => $secretpin
-          ));
-        header('location:login.php');
-        exit;
-      }
-      catch(PDOException $e) {
-        echo $e->getMessage();
-      }
-    }
-  }*/
-
-//  if(isset($_GET['action']) && $_GET['action'] == 'joined') {
-    //$errMsg = 'Registration successfull. Now you can <a href="login.php">login</a>';
-  //}
 ?>
 
 <!DOCTYPE html>
@@ -167,41 +160,35 @@ include "header.php" ;?>
   </div>
   <div class="signup_errors">
           <?php
-             if(isset($_SESSION['empty_fullname'])){
-                 echo $_SESSION['empty_fullname']."</br>";
-                 unset($_SESSION['empty_fullname']);
+             function msg_toggle($sess_name){
+                if(isset($_SESSION[$sess_name])){
+                    echo $_SESSION[$sess_name];
+                    unset($_SESSION[$sess_name]);
+                    return 1;
+                }
              }
-             if(isset($_SESSION['empty_username'])){
-                 echo $_SESSION['empty_username']."</br>";
-                 unset($_SESSION['empty_username']);
-             }
-             if(isset($_SESSION['empty_email'])){
-                 echo $_SESSION['empty_email']."</br>";
-                 unset($_SESSION['empty_email']);
-             }
-             if(isset($_SESSION['incorrect_email'])){
-                 echo $_SESSION['incorrect_email']."</br>";
-                 unset($_SESSION['incorrect_email']);
-             }
-             if(isset($_SESSION['blank_password'])){
-                 echo $_SESSION['blank_password']."</br>";
-                 unset($_SESSION['blank_password']);
-             }
-             if(isset($_SESSION['emailerr'])){
-                 echo $_SESSION['emailerr']."</br>";
-                 unset($_SESSION['emailerr']);
-             }
+             if(msg_toggle('empty_fullname')) echo "</br>";
+             if(msg_toggle('empty_username')) echo "</br>";
+             if(msg_toggle('empty_email')) echo "</br>";
+             if(msg_toggle('incorrect_email')) echo "</br>";
+             if(msg_toggle('blank_password')) echo "</br>";
+             if(msg_toggle('short_password')) echo "</br>";
+             if(msg_toggle('long_password')) echo "</br>";
+             if(msg_toggle('invalid_password')) echo "</br>";
+             if(msg_toggle('password_mismatch')) echo "</br>";
+             if(msg_toggle('emailerr')) echo "</br>";
           ?>
       </div>
   <div class="container">
-    <label for="fullname"><b>Full name</b></label>
-<input type="text" name="fullname" placeholder="Fullname" value="<?php if(isset($_POST['fullname'])) echo $_POST['fullname'] ?>" autocomplete="off" class="box"/><br /><br />
-    <label for="username"><b>Username</b></label>
-<input type="text" name="username" placeholder="Username" value="<?php if(isset($_POST['username'])) echo $_POST['username'] ?>" autocomplete="off" class="box"/><br /><br />
-<label for="email"><b>Email</b></label>
-<input type="text" name="email" placeholder="Email Adress" value="<?php if(isset($_POST['email'])) echo $_POST['email'] ?>" autocomplete="off" class="box"/><br /><br />
-    <label for="password"><b>Password</b></label>
-<input type="password" name="password" placeholder="Password" value="<?php if(isset($_POST['password'])) echo $_POST['password'] ?>" class="box" /><br/><br />
+    <!-- <label for="fullname"><b>Full name</b></label> -->
+<input type="text" name="fullname" id="fullname" placeholder="Fullname" value="<?php msg_toggle('fullname') ?>" autocomplete="off" class="box"/><br /><br />
+    <!-- <label for="username"><b>Username</b></label> -->
+<input type="text" name="username" id="username" placeholder="Username" value="<?php msg_toggle('username')?>" autocomplete="off" class="box"/><br /><br />
+<!-- <label for="email"><b>Email</b></label> -->
+<input type="text" name="email" id="email" placeholder="Email Address" value="<?php msg_toggle('email') ?>" autocomplete="off" class="box"/><br /><br />
+    <!-- <label for="password"><b>Password</b></label> -->
+<input type="password" name="password" id="password" placeholder="Password" class="box" /><br/><br />
+<input type="password" name="password_confirm" id="password_confirm" placeholder="Confirm Password" class="box" /><br/><br />
 <!--     <label for="psw"><b>Repeat Password</b></label>
     <input type="password" placeholder="Repeat password"> -->
     <div class="coursegroup">
@@ -235,4 +222,3 @@ include "header.php" ;?>
 </section>
 </body>
 </html>
-
