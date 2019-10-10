4c068f0d7e00cda7e6ff3a63233f89ec0defbc96
syfon01 <syfonisaac@gmail.com>
2019-10-10 14:05:17 +0100

    navigation bar

--- createclass.php
+++ createclass.php
@@ -1,73 +1,93 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Create Class</title>
-    <link rel = "stylesheet" type = "text/css" href = "createclass.css" />
+    <link rel="stylesheet" type="text/css" href="createclass.css" />
 </head>
+
 <body>
-  
-<div class="container">
-    <form id="form" action="" method="post">
-    <h1>Create a class for your students</h1>
-    <h3 id="subheader">Enter the details about your class, courses and grades</h3>
-    
-    <div>
-        <div id="error"></div>
-        <table class="formtable">
-            <tr class="tablerows">
-              <th>Class Name</th>
-              <th>Course Title</th>
-            </tr>
+    <div class="container">
 
-            <div id="error"></div>
-            <tr >
-                <td><input type="text" id="className" placeholder="Enter Class Name" autocomplete="on"></td>
-                <td><input type="text" id="classTitle" placeholder="Enter Class Title"  autocomplete="on"></td>
-            </tr>
+        <header>
+            <div class="header">
 
-            <tr>
-                <th class="headertwo">Select Class Category</th>
-                <th class="headertwo">Select Course Level</th>
-            </tr>
-              
-            <div id="error"></div>
-            <tr>
-                <td>
-                    <select name="subjects" class="subjects" id="select1">
-                        <option value="">--Please choose an option--</option>
-                        <option value="Web Development">Web Development</option>
-                        <option value="Data Science">Data Science</option>
-                        <option value="AI">Artificial Intelligence</option>
-                        <option value="Machine Learning">Machine Learning</option>
-                        <option value="Oracle DataBase">Oracle DataBase</option>
-                        <option value="Cisco Networking">Cisco Networking</option>
-                        <option value="RedHat Linux">RedHat Linux</option>
-                        <option value="Digital Marketing">Digital Marketing</option>
-                        <option value="Microsoft">Microsoft System Administration</option>
-                    </select>
-                </td>
+                <?php
+                include "header.php"; ?>
+            </div>
+            <script>
+                function myFunction() {
+                    var x = document.getElementById("myTopnav");
+                    if (x.className === "topnav") {
+                        x.className += " responsive";
+                    } else {
+                        x.className = "topnav";
+                    }
+                }
+            </script>
+        </header>
 
-                <td>
-                        <select name="subjects" class="courselevel" id="select1">
+        <form id="form" action="" method="post">
+            <h1>Create a class for your students</h1>
+            <h3 id="subheader">Enter the details about your class, courses and grades</h3>
+
+            <div>
+                <div id="error"></div>
+                <table class="formtable">
+                    <tr class="tablerows">
+                        <th>Class Name</th>
+                        <th>Course Title</th>
+                    </tr>
+
+                    <div id="error"></div>
+                    <tr>
+                        <td><input type="text" id="className" placeholder="Enter Class Name" autocomplete="on"></td>
+                        <td><input type="text" id="classTitle" placeholder="Enter Class Title" autocomplete="on"></td>
+                    </tr>
+
+                    <tr>
+                        <th class="headertwo">Select Class Category</th>
+                        <th class="headertwo">Select Course Level</th>
+                    </tr>
+
+                    <div id="error"></div>
+                    <tr>
+                        <td>
+                            <select name="subjects" class="subjects" id="select1">
+                                <option value="">--Please choose an option--</option>
+                                <option value="Web Development">Web Development</option>
+                                <option value="Data Science">Data Science</option>
+                                <option value="AI">Artificial Intelligence</option>
+                                <option value="Machine Learning">Machine Learning</option>
+                                <option value="Oracle DataBase">Oracle DataBase</option>
+                                <option value="Cisco Networking">Cisco Networking</option>
+                                <option value="RedHat Linux">RedHat Linux</option>
+                                <option value="Digital Marketing">Digital Marketing</option>
+                                <option value="Microsoft">Microsoft System Administration</option>
+                            </select>
+                        </td>
+
+                        <td>
+                            <select name="subjects" class="courselevel" id="select1">
                                 <option value="">--Please choose an option--</option>
                                 <option value="beginner">Beginner</option>
                                 <option value="intermediate">Intermediate</option>
                                 <option value="expert">Expert</option>
-                                </select> 
-                </td>
-            </tr>
-        
-            <tr>
-                <th>Add an Exercise</th>
-                <th>Add a Quiz</th>
-            </tr>
+                            </select>
+                        </td>
+                    </tr>
 
-            <div id="error"></div>
-            <tr>
-                    <td>
+                    <tr>
+                        <th>Add an Exercise</th>
+                        <th>Add a Quiz</th>
+                    </tr>
+
+                    <div id="error"></div>
+                    <tr>
+                        <td>
                             <select name="subjects" class="exercise" id="select2">
                                 <option value="">--Select an option--</option>
                                 <option value="Web Development">Build a Calculator</option>
@@ -77,31 +97,32 @@
                         </td>
                         <td>
                             <select name="subjects" class="quiz" id="select2">
-                                        <option value="">--Select an option--</option>
-                                        <option value="">What do you intend achieving after this course?</option>
-                                        <option value="">What Career path do you desire?</option>
-                            </select> 
-                            </td>                 
-            </tr>
-        </table>
+                                <option value="">--Select an option--</option>
+                                <option value="">What do you intend achieving after this course?</option>
+                                <option value="">What Career path do you desire?</option>
+                            </select>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+            <div>
+                <ul>
+                    <li><label for="fileupload" id="fileupload"> Upload a Course Video</label></li>
+                    <input id="file-upload" type="video" />
+
+                    <li><input type="file" name="fileupload" value="fileupload" id="fileupload" accept="video/*"></li>
+
+                    <li><button value="uploadfile" id="uploadfile">UPLOAD</button></li>
+
+                    <button id="createbutton" type="submit">Create Classroom</button>
+
+                </ul>
+            </div>
+        </form>
     </div>
 
-    <div>
-    <ul>
-        <li><label for="fileupload" id="fileupload"> Upload a Course Video</label></li>
-        <input id="file-upload" type="video"/>
-
-        <li><input type="file" name="fileupload" value="fileupload" id="fileupload" accept="video/*"></li>
-
-        <li><button value="uploadfile" id="uploadfile">UPLOAD</button></li>
-        
-        <button id="createbutton" type="submit">Create Classroom</button>
-
-    </ul>
-    </div>
-</form>
- </div>
-
- <script type="text/javascript" src="createclass.js"></script>
+    <script type="text/javascript" src="createclass.js"></script>
 </body>
+
 </html>
